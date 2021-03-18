a1b1cc9f039de00fa8d5d7ebc202fd1cd13dd006
christianBTI <68340033+christianBTI@users.noreply.github.com>
2021-03-18 10:30:55 -0600

    Principal

---
+++ codigodebarras/Form1.Designer.vb
@@ -0,0 +1,379 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Dim NombreLabel As System.Windows.Forms.Label
+        Dim MarcaLabel As System.Windows.Forms.Label
+        Dim PrecioLabel As System.Windows.Forms.Label
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TxtNombre = New System.Windows.Forms.TextBox()
+        Me.TxtMarca = New System.Windows.Forms.TextBox()
+        Me.TxtPrecio = New System.Windows.Forms.TextBox()
+        Me.TxtCodigo = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BtnGenerar = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.PrintPreviewDialog1 = New System.Windows.Forms.PrintPreviewDialog()
+        Me.CDBDataSet = New codigodebarras.CDBDataSet()
+        Me.ProductoBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.ProductoTableAdapter = New codigodebarras.CDBDataSetTableAdapters.ProductoTableAdapter()
+        Me.TableAdapterManager = New codigodebarras.CDBDataSetTableAdapters.TableAdapterManager()
+        Me.NombreTextBox = New System.Windows.Forms.TextBox()
+        Me.MarcaTextBox = New System.Windows.Forms.TextBox()
+        Me.PrecioTextBox = New System.Windows.Forms.TextBox()
+        NombreLabel = New System.Windows.Forms.Label()
+        MarcaLabel = New System.Windows.Forms.Label()
+        PrecioLabel = New System.Windows.Forms.Label()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.CDBDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ProductoBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(122, 161)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(703, 372)
+        Me.TabControl1.TabIndex = 8
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.Button2)
+        Me.TabPage1.Controls.Add(Me.BtnGenerar)
+        Me.TabPage1.Controls.Add(Me.Label1)
+        Me.TabPage1.Controls.Add(Me.PictureBox1)
+        Me.TabPage1.Controls.Add(Me.Label5)
+        Me.TabPage1.Controls.Add(Me.Label6)
+        Me.TabPage1.Controls.Add(Me.Label7)
+        Me.TabPage1.Controls.Add(Me.Label8)
+        Me.TabPage1.Controls.Add(Me.TxtNombre)
+        Me.TabPage1.Controls.Add(Me.TxtMarca)
+        Me.TabPage1.Controls.Add(Me.TxtPrecio)
+        Me.TabPage1.Controls.Add(Me.TxtCodigo)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(695, 346)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Registro"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.GroupBox1)
+        Me.TabPage2.Controls.Add(Me.Label2)
+        Me.TabPage2.Controls.Add(Me.TextBox1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(695, 346)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Consulta"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(294, 125)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(37, 13)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "Precio"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(294, 70)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(44, 13)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "Nombre"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(68, 125)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(37, 13)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "Marca"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(68, 73)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(40, 13)
+        Me.Label8.TabIndex = 12
+        Me.Label8.Text = "Codigo"
+        '
+        'TxtNombre
+        '
+        Me.TxtNombre.Location = New System.Drawing.Point(374, 70)
+        Me.TxtNombre.Name = "TxtNombre"
+        Me.TxtNombre.Size = New System.Drawing.Size(126, 20)
+        Me.TxtNombre.TabIndex = 2
+        '
+        'TxtMarca
+        '
+        Me.TxtMarca.Location = New System.Drawing.Point(129, 118)
+        Me.TxtMarca.Name = "TxtMarca"
+        Me.TxtMarca.Size = New System.Drawing.Size(132, 20)
+        Me.TxtMarca.TabIndex = 1
+        '
+        'TxtPrecio
+        '
+        Me.TxtPrecio.Location = New System.Drawing.Point(374, 118)
+        Me.TxtPrecio.Name = "TxtPrecio"
+        Me.TxtPrecio.Size = New System.Drawing.Size(126, 20)
+        Me.TxtPrecio.TabIndex = 3
+        '
+        'TxtCodigo
+        '
+        Me.TxtCodigo.Location = New System.Drawing.Point(129, 70)
+        Me.TxtCodigo.Name = "TxtCodigo"
+        Me.TxtCodigo.Size = New System.Drawing.Size(132, 20)
+        Me.TxtCodigo.TabIndex = 0
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(134, 165)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(127, 54)
+        Me.PictureBox1.TabIndex = 16
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 190)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 13)
+        Me.Label1.TabIndex = 17
+        Me.Label1.Text = "Codigo De Barra"
+        '
+        'BtnGenerar
+        '
+        Me.BtnGenerar.Location = New System.Drawing.Point(383, 182)
+        Me.BtnGenerar.Name = "BtnGenerar"
+        Me.BtnGenerar.Size = New System.Drawing.Size(117, 37)
+        Me.BtnGenerar.TabIndex = 4
+        Me.BtnGenerar.Text = "Generar Codigo De Barra"
+        Me.BtnGenerar.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(134, 270)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(127, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Guardar Producto"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(129, 66)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(118, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(52, 69)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Codigo"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(PrecioLabel)
+        Me.GroupBox1.Controls.Add(Me.PrecioTextBox)
+        Me.GroupBox1.Controls.Add(MarcaLabel)
+        Me.GroupBox1.Controls.Add(Me.MarcaTextBox)
+        Me.GroupBox1.Controls.Add(NombreLabel)
+        Me.GroupBox1.Controls.Add(Me.NombreTextBox)
+        Me.GroupBox1.Location = New System.Drawing.Point(31, 140)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(543, 156)
+        Me.GroupBox1.TabIndex = 8
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Resultado De La Busquedad"
+        '
+        'PrintDocument1
+        '
+        '
+        'PrintPreviewDialog1
+        '
+        Me.PrintPreviewDialog1.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PrintPreviewDialog1.Document = Me.PrintDocument1
+        Me.PrintPreviewDialog1.Enabled = True
+        Me.PrintPreviewDialog1.Icon = CType(resources.GetObject("PrintPreviewDialog1.Icon"), System.Drawing.Icon)
+        Me.PrintPreviewDialog1.Name = "PrintPreviewDialog1"
+        Me.PrintPreviewDialog1.Visible = False
+        '
+        'CDBDataSet
+        '
+        Me.CDBDataSet.DataSetName = "CDBDataSet"
+        Me.CDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'ProductoBindingSource
+        '
+        Me.ProductoBindingSource.DataMember = "Producto"
+        Me.ProductoBindingSource.DataSource = Me.CDBDataSet
+        '
+        'ProductoTableAdapter
+        '
+        Me.ProductoTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.ProductoTableAdapter = Me.ProductoTableAdapter
+        Me.TableAdapterManager.UpdateOrder = codigodebarras.CDBDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
+        'NombreLabel
+        '
+        NombreLabel.AutoSize = True
+        NombreLabel.Location = New System.Drawing.Point(126, 48)
+        NombreLabel.Name = "NombreLabel"
+        NombreLabel.Size = New System.Drawing.Size(47, 13)
+        NombreLabel.TabIndex = 7
+        NombreLabel.Text = "Nombre:"
+        '
+        'NombreTextBox
+        '
+        Me.NombreTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.ProductoBindingSource, "Nombre", True))
+        Me.NombreTextBox.Location = New System.Drawing.Point(179, 46)
+        Me.NombreTextBox.Name = "NombreTextBox"
+        Me.NombreTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.NombreTextBox.TabIndex = 8
+        '
+        'MarcaLabel
+        '
+        MarcaLabel.AutoSize = True
+        MarcaLabel.Location = New System.Drawing.Point(377, 49)
+        MarcaLabel.Name = "MarcaLabel"
+        MarcaLabel.Size = New System.Drawing.Size(40, 13)
+        MarcaLabel.TabIndex = 8
+        MarcaLabel.Text = "Marca:"
+        AddHandler MarcaLabel.Click, AddressOf Me.MarcaLabel_Click
+        '
+        'MarcaTextBox
+        '
+        Me.MarcaTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.ProductoBindingSource, "Marca", True))
+        Me.MarcaTextBox.Location = New System.Drawing.Point(423, 46)
+        Me.MarcaTextBox.Name = "MarcaTextBox"
+        Me.MarcaTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.MarcaTextBox.TabIndex = 9
+        '
+        'PrecioLabel
+        '
+        PrecioLabel.AutoSize = True
+        PrecioLabel.Location = New System.Drawing.Point(220, 119)
+        PrecioLabel.Name = "PrecioLabel"
+        PrecioLabel.Size = New System.Drawing.Size(40, 13)
+        PrecioLabel.TabIndex = 9
+        PrecioLabel.Text = "Precio:"
+        '
+        'PrecioTextBox
+        '
+        Me.PrecioTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.ProductoBindingSource, "Precio", True))
+        Me.PrecioTextBox.Location = New System.Drawing.Point(266, 116)
+        Me.PrecioTextBox.Name = "PrecioTextBox"
+        Me.PrecioTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.PrecioTextBox.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1142, 925)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.CDBDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ProductoBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents Button2 As Button
+    Friend WithEvents BtnGenerar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TxtNombre As TextBox
+    Friend WithEvents TxtMarca As TextBox
+    Friend WithEvents TxtPrecio As TextBox
+    Friend WithEvents TxtCodigo As TextBox
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+    Friend WithEvents PrintPreviewDialog1 As PrintPreviewDialog
+    Friend WithEvents CDBDataSet As CDBDataSet
+    Friend WithEvents ProductoBindingSource As BindingSource
+    Friend WithEvents ProductoTableAdapter As CDBDataSetTableAdapters.ProductoTableAdapter
+    Friend WithEvents TableAdapterManager As CDBDataSetTableAdapters.TableAdapterManager
+    Friend WithEvents MarcaTextBox As TextBox
+    Friend WithEvents NombreTextBox As TextBox
+    Friend WithEvents PrecioTextBox As TextBox
+End Class
---
+++ codigodebarras/Form1.vb
@@ -0,0 +1,65 @@
+﻿Imports iTextSharp.text.pdf
+Imports System.Data.OleDb
+Public Class Form1
+    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BtnGenerar.Click
+        Dim bcode As New Barcode128
+        bcode.BarHeight = 50
+        bcode.Code = TxtCodigo.Text
+        bcode.GenerateChecksum = True
+        bcode.CodeType = Barcode128.CODE128
+        Try
+            Dim bm As New Bitmap(bcode.CreateDrawingImage(Color.Black, Color.White))
+            Dim img As Image
+            img = New Bitmap(bm.Width, bm.Height)
+            Dim g As Graphics = Graphics.FromImage(img)
+            g.FillRectangle(New SolidBrush(Color.White), 0, 0, bm.Width, bm.Height)
+            g.DrawImage(bm, 0, 0)
+            PictureBox1.Image = img
+
+        Catch ex As Exception
+            MsgBox("no se pudo generar el codigo de barras")
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.ProductoTableAdapter.InsertQuery(TxtCodigo.Text, TxtNombre.Text, TxtMarca.Text, TxtPrecio.Text)
+        PrintPreviewDialog1.Show()
+    End Sub
+
+    Private Sub PrintDocument1_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles PrintDocument1.PrintPage
+        Dim bm As New Bitmap(PictureBox1.Width, PictureBox1.Height)
+        PictureBox1.DrawToBitmap(bm, New Rectangle(0, 0, bm.Width, bm.Height))
+        e.Graphics.DrawImage(bm, 0, 0)
+    End Sub
+
+    Private Sub ProductoBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs)
+        Me.Validate()
+        Me.ProductoBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.CDBDataSet)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: esta línea de código carga datos en la tabla 'CDBDataSet.Producto' Puede moverla o quitarla según sea necesario.
+        Me.ProductoTableAdapter.Fill(Me.CDBDataSet.Producto)
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_PreviewKeyDown(sender As Object, e As PreviewKeyDownEventArgs) Handles TextBox1.PreviewKeyDown
+        If e.KeyCode = Keys.Enter Then
+            Me.ProductoTableAdapter.FillBy(Me.CDBDataSet.Producto, TextBox1.Text)
+        End If
+    End Sub
+
+    Private Sub MarcaLabel_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
